77d52eb8691609c94e609e49521c869ca9fa9811
Aron Culotta <aronwc@gmail.com>
2014-12-08 15:19:41 -0600

    write diagnostics to output file

--- brandelion/cli/diagnose.py
+++ brandelion/cli/diagnose.py
@@ -3,7 +3,7 @@
 """Run diagnostics on a dataset.
 
 usage:
-    brandelion diagnose --network --brand-followers <file> --exemplar-followers <file> --validation <file> [--network-method <string>]
+    brandelion diagnose --network --brand-followers <file> --exemplar-followers <file> --validation <file> --output <file> [--network-method <string>]
 
 Options
     -h, --help
@@ -11,11 +11,13 @@ Options
     --exemplar-followers <file>   File containing follower data for exemplar accounts.
     --network-method <string>     Method to do text analysis [default: jaccard]
     -n, --network                 Analyze followers.
+    -o, --output <file>           File to store results
     -t, --text                    Analyze text.
     -v, --validation <file>       File containing third-party scores for each brand by Twitter name, (e.g., surveys), for comparison.
 """
 
 from docopt import docopt
+import os
 import scipy.stats as scistat
 import random
 
@@ -45,39 +47,45 @@ def diagnose_text(brand_tweets_file, exemplar_tweets_file, sample_tweets_file, v
     pass
 
 
-def correlation_by_exemplar(brands, exemplars, validation_scores, analyze_fn_str):
+def correlation_by_exemplar(brands, exemplars, validation_scores, analyze_fn_str, outf):
     """ Report the overall correlation with the validation scores using each exemplar in isolation. """
     analyze_fn = getattr(analyze, analyze_fn_str)
     keys = sorted(k for k in validation_scores.keys() if k in set(x[0] for x in brands))
     truth = [validation_scores[k] for k in keys]
-    sample = random.sample(keys, 10)
     result = {}
-    print 'exemplar\tcorr\tn_followers\texamples\n'
+    outf.write('exemplar\tcorr\tn_followers\n')
+    outf.flush()
     for exemplar in exemplars:
         single_exemplar = {exemplar: exemplars[exemplar]}
         social_scores = analyze_fn(brands, single_exemplar)
         predicted = [social_scores[k] for k in keys]
-        print '%10s\t%.3f\t%d\t%s' % (exemplar, scistat.pearsonr(predicted, truth)[0],
-                                      len(exemplars[exemplar]),
-                                      ' '.join('%.2g' % social_scores[k] for k in sample))
+        outf.write('%s\t%g\t%d\n' % (exemplar, scistat.pearsonr(predicted, truth)[0], len(exemplars[exemplar])))
+        outf.flush()
         result[exemplar] = scistat.pearsonr(predicted, truth)[0]
+    outf.close()
     return result
 
 
-def diagnose_followers(brand_follower_file, exemplar_follower_file, validation_file, analyze_fn):
+def mkdirs(filename):
+    report.mkdirs(os.path.dirname(filename))
+
+
+def diagnose_followers(brand_follower_file, exemplar_follower_file, validation_file, analyze_fn, output_file):
+    mkdirs(output_file)
+    outf = open(output_file, 'wt')
     brands = analyze.read_follower_file(brand_follower_file).items()
     exemplars = analyze.read_follower_file(exemplar_follower_file)
     print 'read follower data for %d exemplars' % (len(exemplars))
     scores = report.read_scores(validation_file)
-    return correlation_by_exemplar(brands, exemplars, scores, analyze_fn)
+    return correlation_by_exemplar(brands, exemplars, scores, analyze_fn, outf)
 
 
 def main():
     args = docopt(__doc__)
     print args
-    if args['--network']:
-        diagnose_followers(args['--brand-followers'], args['--exemplar-followers'], args['--validation'], args['--network-method'])
-    if args['--text']:
+    if '--network' in args:
+        diagnose_followers(args['--brand-followers'], args['--exemplar-followers'], args['--validation'], args['--network-method'], args['--output'])
+    if '--text' in args:
         diagnose_text(args['--brand-tweets'], args['--exemplar-tweets'], args['--sample-tweets'], args['--validation'], args['--text-method'])
 
 
