90f0506c9f62c2617818da68b15c3c3ce952aa62
Aron Culotta <aronwc@gmail.com>
2015-11-10 09:44:11 -0600

    bump version

--- brandelion/__init__.py
+++ brandelion/__init__.py
@@ -10,7 +10,7 @@ except ImportError:
 
 __author__ = 'Aron Culotta'
 __email__ = 'aronwc@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 config = ConfigParser()
 if 'BRANDELION_CFG' in os.environ:
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='brandelion',
-    version='0.1.5',
+    version='0.1.6',
     description='Social media brand analytics',
     long_description=readme + '\n\n' + history,
     author='Aron Culotta',
